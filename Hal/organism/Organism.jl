5303d379ef3269eedc69dadebd59c47fdbd2dc4e
Jordan Lazzaro <jordantlazzaro@gmail.com>
2018-10-10 02:59:45 -0400

    Add files via upload
    Adding Types for evolutionary algorithms

---
+++ Hal/organism/Organism.jl
@@ -0,0 +1,6 @@
+"""
+
+An Organism is a Type that contains data representing a genotype
+and corresponding phenotype.
+
+"""
---
+++ Hal/population/Population.jl
@@ -0,0 +1,7 @@
+"""
+
+A Population is a Type that contains a collection of Organisms, as well
+as information about those Organisms, that is used by other Types in the
+Hal.jl library.
+
+"""

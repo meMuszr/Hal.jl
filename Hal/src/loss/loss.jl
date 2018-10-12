04fdc5ffd94d8ff32b84752344badfcd3dae4efc
Jordan Lazzaro <jordantlazzaro@gmail.com>
2018-10-12 02:08:03 -0400

    added loss

---
+++ Hal/src/loss/loss.jl
@@ -0,0 +1,6 @@
+"""
+
+A Loss is a Type associated with a loss function
+that returns a Float64 value of the final error of a Circuit
+
+"""
--- Hal/src/optimizer/optimizer.jl
+++ Hal/src/optimizer/optimizer.jl
@@ -1,6 +1,6 @@
 """
 
-An Optimizer is a Type that optimizes the parameters of all the Layers 
-in a CognitiveCircuit.
+An Optimizer is a Type that takes a Circuit, and an error value associated with 
+that Circuit, and optimizes the parameters of all the Layers in the Circuit.
 
 """
